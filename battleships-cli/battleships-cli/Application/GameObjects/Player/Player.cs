ddc131b9568053e665a10f335d1e727509d9b76e
onosume <main.matthewtinn@gmail.com>
2016-03-16 06:49:24 +0000

    Added Player

---
+++ battleships-cli/battleships-cli/Application/GameObjects/Player/Player.cs
@@ -0,0 +1,37 @@
+﻿// battlships-cli
+// Player.cs
+// Author: Matthew Tinn
+
+namespace battleships_cli.Application.GameObjects.Player
+{
+    /// <summary>
+    /// The player class
+    /// Holds the player's score and number of cannonballs remaining
+    /// </summary>
+    public class Player
+    {
+        private int score { get; set; }
+        private int cannonballs { get; set; }
+
+        public Player(int cannonballs)
+        {
+            Init(cannonballs);
+        }
+        
+        private void Init(int cannonballs)
+        {
+            score = 0;
+            this.cannonballs = cannonballs;
+        }
+
+        public void TakeShot()
+        {
+            cannonballs--;
+        }
+
+        public void AddToScore(int points)
+        {
+            score += points;
+        }
+    }
+}
