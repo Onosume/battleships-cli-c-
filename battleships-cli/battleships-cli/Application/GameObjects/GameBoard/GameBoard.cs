b5bd4309508253be01715b25b98561a73e7b3484
onosume <main.matthewtinn@gmail.com>
2016-03-19 14:47:04 +0000

    Add Game Board

---
+++ battleships-cli/battleships-cli/Application/GameObjects/GameBoard/GameBoard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace battleships_cli.Application.GameObjects.GameBoard
+{
+    public class GameBoard
+    {
+        private const int BOARD_WIDTH = 10;
+        private const int BOARD_HEIGHT = 10;
+        private const char HIT_MARKER = 'H';
+        private const char MISS_MARKER = 'M';
+        private char[,] board;
+
+        public GameBoard()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            board = new char[BOARD_WIDTH, BOARD_HEIGHT];
+
+            for (int i = 0; i < BOARD_WIDTH; i++)
+            {
+                for (int j = 0; j < BOARD_HEIGHT; j++)
+                {
+                    board[i, j] = 'O';
+                }
+            }
+        }
+
+        public void SetCell(int posX, int posY, char status)
+        {
+            board[posX, posY] = status;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < BOARD_WIDTH; i++)
+            {
+                for (int j = 0; j < BOARD_HEIGHT; j++)
+                {
+                    Console.Write(board[i, j]);
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
