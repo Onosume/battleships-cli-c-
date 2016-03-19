0cb41265d11b01fb7fe170c68eaefce3990cdae1
onosume <main.matthewtinn@gmail.com>
2016-03-19 21:09:51 +0000

    Add comments to player

--- battleships-cli/battleships-cli/Application/GameObjects/Player/Player.cs
+++ battleships-cli/battleships-cli/Application/GameObjects/Player/Player.cs
@@ -17,22 +17,37 @@ namespace battleships_cli.Application.GameObjects.Player
         public int Score { get { return score; } }
         public int Cannonballs { get { return cannonballs; } }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="numCannonballs">Player's maximum number of cannonballs</param>
         public Player(int numCannonballs)
         {
             Init(numCannonballs);
         }
-        
+
+        /// <summary>
+        /// Initialise the player
+        /// </summary>
+        /// <param name="numCannonballs">Player's maximum number of cannonballs</param>
         private void Init(int numCannonballs)
         {
             score = 0;
             cannonballs = numCannonballs;
         }
 
+        /// <summary>
+        /// Decrement player cannonballs
+        /// </summary>
         public void TakeShot()
         {
             cannonballs--;
         }
 
+        /// <summary>
+        /// Add specific value to player score
+        /// </summary>
+        /// <param name="points">Value to add</param>
         public void AddToScore(int points)
         {
             score += points;
